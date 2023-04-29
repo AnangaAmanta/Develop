79eeea4951f5dc1af8935a3d7c0f30744ef07dd6
AnangaAmanta <ananga.amanta@gmail.com>
2023-04-29 13:52:10 +0530

    Dev Branch

---
+++ Develop/GetInputFromUser.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GetInputFromUser {
+	
+	public static void main(String[] args) {
+		int a;
+		String b;
+		float c;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a String");
+		
+		b = sc.nextLine();
+		
+		System.out.println("String entered is: "+b);
+
+		
+		System.out.println("Enter a Float");
+		
+		c = sc.nextFloat();
+		
+		System.out.println("Float entered is: "+c);
+		
+		sc.close();
+		
+	}
+
+}
